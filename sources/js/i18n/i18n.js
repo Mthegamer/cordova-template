715d4dd0d35a55b8ee5f265c70cdcf3f01ffed26
bob <9wishdrum0>
2014-06-17 15:24:39 +0200

    design, info in demo app

--- sources/js/i18n/i18n.js
+++ sources/js/i18n/i18n.js
@@ -2,7 +2,22 @@
  * provides localized texts. in fact, currently jquery.i18n.js does all the work.
  * configure texts in jquery.XX.json to support languages and texts in project.i18n.XX.json for the supported languages
  *
+ * part of the cordova template at https://github.com/sk3l3t0r/cordova-template
  * author: android0x17@gmail.com
+ *
+ * Copyright 2014 android0x17
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 function I18n() {
